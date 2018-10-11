dc37f24718f0069c3160e5b98132a6302586aaca
sawepeter <sawepeter6@gmail.com>
2018-10-11 13:32:50 +0300

    error debugging :tada:

--- app/src/main/java/com/example/devsawe/devops/adapter/AlbumAdapter.java
+++ app/src/main/java/com/example/devsawe/devops/adapter/AlbumAdapter.java
@@ -4,11 +4,16 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.devsawe.devops.Model.Album;
 import com.example.devsawe.devops.R;
 
@@ -27,13 +32,30 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyViewHolder
     }
 
     @Override
-    public void onBindViewHolder(@NonNull AlbumAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final AlbumAdapter.MyViewHolder holder, int position) {
+        Album album = albumList.get(position);
+        holder.title.setText(album.getName());
+        holder.count.setText(album.getNumOfSongs() + "songs");
+
+        //loading album cover using Glide library
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+        
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(holder.overflow);
+            }
+        });
 
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
+    private void showPopupMenu(ImageView overflow) {
+        //inflate menu
+        PopupMenu popupMenu = new PopupMenu(mContext, overflow);
+        MenuInflater inflater = popupMenu.getMenuInflater();
+        inflater.inflate(R.menu.menu_album, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popupMenu.show();
     }
     public AlbumAdapter(Context mContext, List<Album> albumList){
         this.mContext = mContext;
@@ -51,5 +73,27 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.MyViewHolder
             thumbnail = itemView.findViewById(R.id.thumbnail);
             overflow = itemView.findViewById(R.id.overflow);
         }
+    }
+
+    private class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_add_favourite:
+                    Toast.makeText(mContext, "Add to Favourite", Toast.LENGTH_SHORT).show();
+                case R.id.action_play_next:
+                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return albumList.size();
     }
 }
